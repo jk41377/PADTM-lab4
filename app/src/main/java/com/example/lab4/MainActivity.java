751e67f426ee19da32bfe3566a883e00ba600c79
kamiljalowiczor <kamil.jalowiczor@gmail.com>
2020-06-07 21:37:34 +0200

    add SimpleCursorAdapter for ListView

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -9,18 +9,22 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<String> target;
-    private ArrayAdapter adapter;
+    private SimpleCursorAdapter adapter;
+    private MySQLite db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.db = new MySQLite(this);
 
         String[] values = new String[] { "Pies", "Kot", "Koń", "Gołąb",
         "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };
@@ -28,7 +32,17 @@ public class MainActivity extends AppCompatActivity {
         this.target = new ArrayList<String>();
         this.target.addAll(Arrays.asList(values));
 
-        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+        this.adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                db.lista(),
+                new String[] {"_id", "gatunek"},
+                new int[] {android.R.id.text1,
+                        android.R.id.text2},
+
+                SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE
+        );
+
 
         ListView listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter(this.adapter);
